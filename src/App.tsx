5aed84cba709e24597a72253d310303904c02f55
Madouss-Traore <madouss.traore.dev@gmail.com>
2025-11-09 13:16:20 +0100

    feat: add basic video player

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import "./App.css";
+import { Video } from "reactjs-media";
+
 
 function App() {
   return (
     <div className="App">
       <h1>Madouss Player</h1>
       <p>Custom video player</p>
+      <Video
+        src="/nature.mp4"
+        controls={true}
+        width={800}
+        height={450}
+        poster="/nature-image.png"
+        />
+        
     </div>
   );
 }
